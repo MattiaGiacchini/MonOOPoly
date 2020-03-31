57b2986bd736b40225ed8b7a9930a16c6053c84a
Daniele <dejackdani@gmail.com>
2020-03-31 23:15:12 +0200

    new class Tile.java added

---
+++ monoopoly/src/main/java/monoopoly/model/item/Tile.java
@@ -0,0 +1,15 @@
+package monoopoly.model.item;
+
+
+/**
+ *	This interface represents a generic
+ * 	Table's tile.
+ */
+public interface Tile {
+	/**
+	 *  This is method is used to understand if the 
+	 *  Tile is purchasable
+	 *  @return true if tile is purchasable, false if not
+	 */
+	boolean isPurchasable();
+}

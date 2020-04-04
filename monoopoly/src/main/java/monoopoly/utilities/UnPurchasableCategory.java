cee036b2e478443320dc6e043d736f3e80ee93e9
Daniele <dejackdani@gmail.com>
2020-04-04 18:26:34 +0200

    Enum UnPurchasableCategory added

---
+++ monoopoly/src/main/java/monoopoly/utilities/UnPurchasableCategory.java
@@ -0,0 +1,13 @@
+package monoopoly.utilities;
+/**
+ *	This enum lists the possible Category of UnPurchasable Tile
+ */
+public enum UnPurchasableCategory {
+	START,
+	DECK_CALAMITY,
+	DECK_PROBABILITY,
+	DECK_UNEXPECTED,
+	JAIL,
+	GO_TO_JAIL,
+	FREE_PARKING;
+}

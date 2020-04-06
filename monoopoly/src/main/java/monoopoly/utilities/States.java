84bd4f1f35eca8e42a502a8dae2f071662c351db
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-04-06 18:36:02 +0200

    Changed two names of the Enum refactoring the code

--- monoopoly/src/main/java/monoopoly/utilities/States.java
+++ monoopoly/src/main/java/monoopoly/utilities/States.java
@@ -3,15 +3,15 @@ package monoopoly.utilities;
  *	This enum lists the possible states in which a character can be during a game
  */
 public enum States {
+	IN_GAME,
+	PRISONED,
 	SLEEPING,
 	MOVING,
-	STANDING,
 	OVERSEEING_PROPERTY,
 	BUYING,
 	TRADING,
 	MORTGAGING,
 	PAYING,
 	ENDING,
-	INPRISONED,
 	BROKE;
 }

3f7f1225592c1a20b70f0251bc674bab5143f20d
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-04-27 22:39:27 +0200

    Added method to initialize the board; added JavaDoc

--- monoopoly/src/main/java/monoopoly/view/ViewUtilities.java
+++ monoopoly/src/main/java/monoopoly/view/ViewUtilities.java
@@ -1,7 +1,25 @@
 package monoopoly.view;
 
+import javafx.scene.layout.GridPane;
+
 public interface ViewUtilities {
 
-	int getBoardPosition(int x, int y, int totCells);
+	/**
+	 * This method return the linear position of a cell in a grid
+	 * 
+	 * @param x        value
+	 * @param y        value
+	 * @param totCells number of cells
+	 * @return the linear position
+	 */
+	public int getBoardPosition(int x, int y, int totCells);
+
+	/**
+	 * This method initializes the game board setting all the names of the
+	 * {@link Tile}s
+	 * 
+	 * @param pane the board gridPane
+	 */
+	public void initializeBoard(GridPane pane);
 
 }
--- monoopoly/src/main/java/monoopoly/view/ViewUtilitiesImpl.java
+++ monoopoly/src/main/java/monoopoly/view/ViewUtilitiesImpl.java
@@ -1,14 +1,53 @@
 package monoopoly.view;
 
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
+import monoopoly.model.item.Table;
+import monoopoly.model.item.TableImpl;
+import monoopoly.model.item.Tile;
+
 public class ViewUtilitiesImpl implements ViewUtilities {
+
+	private Table table = new TableImpl();
 
 	@Override
 	public int getBoardPosition(int x, int y, int totCells) {
 
 		if (x == 0 || y == totCells / 2) {
-			return totCells  - (x + y);
+			return totCells - (x + y);
 		} else {
 			return totCells + (x + y);
 		}
 	}
+
+	@Override
+	public void initializeBoard(GridPane pane) {
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 11; j++) {
+				if (i == 0 || j == 0 || i == 10 || j == 10) {
+					Button button = this.getChild(pane, i, j);
+					String name = table.getTile(this.getBoardPosition(j, i, 20)).getName().toUpperCase();
+					button.setText(name);
+				}
+
+			}
+		}
+	}
+
+	private Button getChild(GridPane pane, int row, int column) {
+		for (Node button : pane.getChildren()) {
+			if (GridPane.getRowIndex(button) == row && GridPane.getColumnIndex(button) == column)
+				return (Button) button;
+		}
+		return null;
+	}
+
 }

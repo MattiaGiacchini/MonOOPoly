2d07e898b020ca4040c170325c90f151966470c4
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-04-27 22:39:54 +0200

    Improved board controller

--- monoopoly/src/main/java/monoopoly/view/board/MainBoardController.java
+++ monoopoly/src/main/java/monoopoly/view/board/MainBoardController.java
@@ -1,6 +1,11 @@
 package monoopoly.view.board;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -35,12 +40,63 @@ public class MainBoardController implements Initializable {
 	@FXML
 	private Label currentPlayerBalance;
 
+	/*
+	 * Player's data fields (name and balance)
+	 */
+
+	@FXML
+	private Label playerName0;
+
+	@FXML
+	private Label playerName1;
+
+	@FXML
+	private Label playerName2;
+
+	@FXML
+	private Label playerName3;
+
+	@FXML
+	private Label playerName4;
+
+	@FXML
+	private Label playerName5;
+
+	@FXML
+	private Label playerBalance0;
+
+	@FXML
+	private Label playerBalance1;
+
+	@FXML
+	private Label playerBalance2;
+
+	@FXML
+	private Label playerBalance3;
+
+	@FXML
+	private Label playerBalance4;
+
+	@FXML
+	private Label playerBalance5;
+
+	private List<Label> playerNames;
+
 	private ViewUtilities utilities = new ViewUtilitiesImpl();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.currentPlayer.setText("Mattia");
-		this.currentPlayerBalance.setText("€ 500,00");
+		this.utilities.initializeBoard(gridPane);
+		this.playerNames = new ArrayList<Label>(
+				Arrays.asList(playerName0, playerName1, playerName2, playerName3, playerName4, playerName5));
+		Map<Integer, Double> balance = new HashMap<Integer, Double>();
+		balance.put(0, 500.00);
+		balance.put(1, 1500.00);
+		balance.put(2, 2500.00);
+		balance.put(3, 3500.00);
+		balance.put(4, 4500.00);
+		balance.put(5, 6500.00);
+		this.updatePlayerBalances(balance);
 	}
 
 	@FXML
@@ -49,6 +105,35 @@ public class MainBoardController implements Initializable {
 		int index = this.utilities.getBoardPosition(GridPane.getColumnIndex(myButton), GridPane.getRowIndex(myButton),
 				this.gridPane.getRowCount() + this.gridPane.getColumnCount() - 2);
 		System.out.println("\t " + index);
+	}
+
+	@FXML
+	public void rollDicesButtonPressed() {
+
+	}
+
+	@FXML
+	public void nextTurnButtonPressed() {
+
+	}
+
+	@FXML
+	public void surrenderButtonPressed() {
+
+	}
+
+	public void updatePlayerBalances(final Map<Integer, Double> balances) {
+		balances.forEach((K, V) -> {
+			this.playerNames.get(K).setText(String.format("%.0f", V));
+		});
+	}
+
+	public void setPlayerNames(final Map<Integer, String> names) {
+
+	}
+
+	public void showDices(final int dice1, final int dice2) {
+
 	}
 
 }

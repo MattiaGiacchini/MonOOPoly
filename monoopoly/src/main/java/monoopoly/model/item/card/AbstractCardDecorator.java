76685af9aa7c80a0b8d0815eee3dec58e6ca5b1b
Daniele <dejackdani@gmail.com>
2020-04-28 17:24:20 +0200

    AbstractCardDecorator/CardImpl: added control input

--- monoopoly/src/main/java/monoopoly/model/item/card/AbstractCardDecorator.java
+++ monoopoly/src/main/java/monoopoly/model/item/card/AbstractCardDecorator.java
@@ -1,5 +1,6 @@
 package monoopoly.model.item.card;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -65,5 +66,41 @@ public abstract class AbstractCardDecorator implements Card {
 	public Optional<Map<Integer, Integer>> getNumberOfBuildingsToRemove() {
 		return this.decoratedCard.getNumberOfBuildingsToRemove();
 	}
+
+	protected Integer getIdPlayerWhoHasDrow() {
+		if(this.decoratedCard instanceof CardImpl) {
+			return ((CardImpl)this.decoratedCard).IdPlayerWhoHasDraw;
+		} else if(AbstractCardDecorator.class.isAssignableFrom(this.decoratedCard.getClass())) {
+			return ((AbstractCardDecorator)this.decoratedCard).getIdPlayerWhoHasDrow();
+		}   
+		throw new IllegalStateException();
+	}
+	
+	protected Map<Integer, Double> getActualPlayersBalance() {
+		if(this.decoratedCard instanceof CardImpl) {
+			return Collections.unmodifiableMap(((CardImpl)this.decoratedCard).actualPlayersBalance);
+		} else if(AbstractCardDecorator.class.isAssignableFrom(this.decoratedCard.getClass())) {
+			return Collections.unmodifiableMap(((AbstractCardDecorator)this.decoratedCard).getActualPlayersBalance());
+		}   
+		throw new IllegalStateException();
+	}
+
+	protected Double getActualBankBalance() {
+		if(this.decoratedCard instanceof CardImpl) {
+			return ((CardImpl)this.decoratedCard).actualBankBalance;
+		} else if(AbstractCardDecorator.class.isAssignableFrom(this.decoratedCard.getClass())) {
+			return ((AbstractCardDecorator)this.decoratedCard).getActualBankBalance();
+		} 
+		throw new IllegalStateException();
+	}
+
+	protected Map<Integer, Integer> getActualPlayersPosition() {
+		if(this.decoratedCard instanceof CardImpl) {
+			return Collections.unmodifiableMap(((CardImpl)this.decoratedCard).actualPlayersPosition);
+		} else if(AbstractCardDecorator.class.isAssignableFrom(this.decoratedCard.getClass())) {
+			return Collections.unmodifiableMap(((AbstractCardDecorator)this.decoratedCard).getActualPlayersPosition());
+		}   
+		throw new IllegalStateException();
+	}	
 
 }
--- monoopoly/src/main/java/monoopoly/model/item/card/CardImpl.java
+++ monoopoly/src/main/java/monoopoly/model/item/card/CardImpl.java
@@ -6,11 +6,20 @@ import java.util.Optional;
 
 import monoopoly.model.item.Tile.Category;
 
-public class CardImpl implements Card {
+public final class CardImpl implements Card {
+
+	// values necessary to create any Card decoration 
+	protected final Integer IdPlayerWhoHasDraw;
+	protected final Map<Integer, Double> actualPlayersBalance;
+	protected final Double actualBankBalance;
+	protected final Map<Integer,Integer> actualPlayersPosition;
 	
+	// values necessary to create any Card
 	private final Integer cardNumber;
 	private final String description;
 	private final Category originDeck;
+	
+	// output values generated on the various decorator
 	private final Optional<Map<Integer, Double>> valueToApplyOnPlayersBalance;
 	private final Optional<Double> valueToApplyOnBankBalance;
 	private final boolean goToJail;
@@ -24,6 +33,11 @@ public class CardImpl implements Card {
 		private Integer cardNumber;
 		private String description;
 		private Category originDeck;
+		private Integer IdPlayerWhoHasDraw;
+		private Map<Integer, Double> actualPlayersBalance;
+		private Double actualBankBalance;
+		private Map<Integer,Integer> actualPlayersPosition;
+		
 		private Optional<Map<Integer, Double>> valueToApplyOnPlayersBalance;
 		private Optional<Double> valueToApplyOnBankBalance;
 		private boolean goToJail;
@@ -33,15 +47,23 @@ public class CardImpl implements Card {
 		private Optional<Map<Integer, Integer>> buildingsToRemove;
 		
 		public Builder() {
-			this.valueToApplyOnBankBalance = Optional.empty();
+			super();
+			this.cardNumber = null;
+			this.description = null;
+			this.originDeck = null;
+			this.IdPlayerWhoHasDraw = null;
+			this.actualPlayersBalance = null;
+			this.actualBankBalance = null;
+			this.actualPlayersPosition = null;
 			this.valueToApplyOnPlayersBalance = Optional.empty();
+			this.valueToApplyOnBankBalance = Optional.empty();
 			this.goToJail = false;
 			this.exitFromJail = false;
 			this.maintainable = false;
 			this.moveToPosition = Optional.empty();
 			this.buildingsToRemove = Optional.empty();
 		}
-		
+
 		public Builder cardNumber(Integer number) {
 			Objects.requireNonNull(number, "the Card number has null value");
 			if(number < 0) {
@@ -63,21 +85,63 @@ public class CardImpl implements Card {
 			return this;
 		}
 		
+		public Builder idPlayerWhoHasDraw(Integer value) {
+			Objects.requireNonNull(value, "the id player cannot a null value");
+			this.IdPlayerWhoHasDraw = value;
+			return this;
+		}
+
+		public Builder actualPlayersBalance(Map<Integer, Double> map){
+			Objects.requireNonNull(map, "the map of actual Players Balance cannot a null value");
+			map.entrySet().forEach(x->this.doubleChecker(x.getValue()));
+			this.actualPlayersBalance = map;
+			return this;
+		}
+		
+		public Builder actualBankBalance(Double value) {
+			Objects.requireNonNull(value, "the bank value cannot a null value");
+			this.doubleChecker(value);
+			this.actualBankBalance = value;
+			return this;
+		}
+		
+		public Builder actualPlayersPosition(Map<Integer,Integer> map) {
+			Objects.requireNonNull(map, "the map of actual player position cannot a null value");
+			this.actualPlayersPosition = map;
+			return this;
+		}
+		
 		public CardImpl build() {
 			if(Objects.isNull(this.cardNumber) ||
 			   Objects.isNull(this.description) ||
-			   Objects.isNull(this.originDeck)) {
+			   Objects.isNull(this.originDeck) ||
+			   Objects.isNull(this.IdPlayerWhoHasDraw) ||
+			   Objects.isNull(this.actualBankBalance) ||
+			   Objects.isNull(this.actualPlayersBalance) ||
+			   Objects.isNull(this.actualPlayersPosition)) {
 				throw new IllegalStateException("missing construction elements");
 			}
 			return new CardImpl(this);
+		}
+		
+		private void doubleChecker(Double value) {
+			if(Objects.nonNull(value) && 
+			   value.isInfinite() || 
+			   value.isNaN()) {
+				throw new IllegalArgumentException("The double value hasn't a correnct format");
+			}
 		}
 	}
 
 	private CardImpl(Builder builder) {
 		super();
+		this.IdPlayerWhoHasDraw 		  = builder.IdPlayerWhoHasDraw;
 		this.cardNumber	 				  = builder.cardNumber;
-		description 					  = builder.description;
+		this.description				  = builder.description;
 		this.originDeck 				  = builder.originDeck;
+		this.actualPlayersPosition 		  = builder.actualPlayersPosition;
+		this.actualPlayersBalance 		  = builder.actualPlayersBalance;
+		this.actualBankBalance 			  = builder.actualBankBalance;
 		this.valueToApplyOnPlayersBalance = builder.valueToApplyOnPlayersBalance;
 		this.valueToApplyOnBankBalance 	  = builder.valueToApplyOnBankBalance;
 		this.goToJail 					  = builder.goToJail;
@@ -136,6 +200,5 @@ public class CardImpl implements Card {
 	public Optional<Map<Integer, Integer>> getNumberOfBuildingsToRemove() {
 		return this.buildingsToRemove;
 	}
-
-		
+	
 }

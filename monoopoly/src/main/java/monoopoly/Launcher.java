6da1836a378474c19ba860c897efc63ee6bfabca
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-04-29 20:44:44 +0200

    Created Main and Launcher class

---
+++ monoopoly/src/main/java/monoopoly/Launcher.java
@@ -0,0 +1,17 @@
+package monoopoly;
+
+/**
+ * This class represents the Launcher of the system
+ */
+public class Launcher {
+
+	private Launcher() {
+	}
+
+	/**
+	 * @param args unused
+	 */
+	public static void main(final String[] args) {
+		Main.main(args);
+	}
+}

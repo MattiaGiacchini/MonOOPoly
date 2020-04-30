1a4ab1f9b88765ffe4a5017bc19af04a8984981a
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-04-30 18:30:56 +0200

    Moved controller methods into sub-classes (divided view in many controllers)

--- monoopoly/src/main/java/monoopoly/view/board/MainBoardController.java
+++ monoopoly/src/main/java/monoopoly/view/board/MainBoardController.java
@@ -1,11 +1,6 @@
 package monoopoly.view.board;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -13,16 +8,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import monoopoly.view.controller.PlayerViewControllerImpl;
 import monoopoly.view.utilities.ViewUtilities;
 import monoopoly.view.utilities.ViewUtilitiesImpl;
 
 public class MainBoardController implements Initializable {
-
-	@FXML
-	private GridPane gridPane;
 
 	@FXML
 	private Button rollDices;
@@ -37,95 +27,18 @@ public class MainBoardController implements Initializable {
 	private Label timer;
 
 	@FXML
-	private Label currentPlayer;
-
-	@FXML
-	private Label currentPlayerBalance;
-
-	/*
-	 * Player's data fields (name and balance)
-	 */
-
-	@FXML
-	private Label playerName0;
-
-	@FXML
-	private Label playerName1;
-
-	@FXML
-	private Label playerName2;
-
-	@FXML
-	private Label playerName3;
-
-	@FXML
-	private Label playerName4;
-
-	@FXML
-	private Label playerName5;
-
-	@FXML
-	private Label playerBalance0;
-
-	@FXML
-	private Label playerBalance1;
-
-	@FXML
-	private Label playerBalance2;
-
-	@FXML
-	private Label playerBalance3;
-
-	@FXML
-	private Label playerBalance4;
-
-	@FXML
-	private Label playerBalance5;
-
-	private List<Label> playerNames;
-	private List<Label> playerBalances;
+	private PlayerViewControllerImpl currentPlayerController;
 
 	private ViewUtilities utilities = new ViewUtilitiesImpl();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		this.utilities.initializeBoard(gridPane);
-		this.playerNames = new ArrayList<Label>(
-				Arrays.asList(playerName0, playerName1, playerName2, playerName3, playerName4, playerName5));
-		this.playerBalances = new ArrayList<Label>(Arrays.asList(playerBalance0, playerBalance1, playerBalance2,
-				playerBalance3, playerBalance4, playerBalance5));
-		// TODO remove those things, used as test
-		Map<Integer, String> name = new HashMap<Integer, String>();
-		name.put(0, "Mattia");
-		name.put(1, "Aiman");
-		name.put(2, "Daniele");
-		name.put(3, "Cristian");
-		name.put(4, "Viroli");
-		name.put(5, "");
-
-		Map<Integer, Double> balance = new HashMap<Integer, Double>();
-		balance.put(0, 500.00);
-		balance.put(1, 1500.00);
-		balance.put(2, 2500.00);
-		balance.put(3, 3500.00);
-		balance.put(4, 4500.00);
-		balance.put(5, 0.00);
-		this.updatePlayerBalances(balance);
-		this.setPlayerNames(name);
+		// TODO
 	}
 
 	@FXML
 	public void quitButtonPressed(ActionEvent event) {
-
 		this.utilities.closeApp(event);
-	}
-
-	@FXML
-	public void cellButtonPressed(ActionEvent event) {
-		Button myButton = (Button) event.getSource();
-		int index = this.utilities.getBoardPosition(GridPane.getColumnIndex(myButton), GridPane.getRowIndex(myButton),
-				this.gridPane.getRowCount() + this.gridPane.getColumnCount() - 2);
-		System.out.println("\t " + index);
 	}
 
 	@FXML
@@ -142,33 +55,7 @@ public class MainBoardController implements Initializable {
 
 	@FXML
 	public void surrenderButtonPressed() {
-
-	}
-
-	@FXML
-	public void displayPlayerPropertiesButtonClicked(MouseEvent event) {
-		HBox box = (HBox) event.getSource();
-		this.currentPlayer.setText(box.getId());
-	}
-
-	public void updatePlayerBalances(final Map<Integer, Double> balances) {
-		balances.forEach((K, V) -> {
-			if (V.equals(0.0)) {
-				this.playerBalances.get(K).setText("");
-			} else {
-				this.playerBalances.get(K).setText(String.format("%.0f", V));
-			}
-		});
-	}
-
-	public void setPlayerNames(final Map<Integer, String> names) {
-		names.forEach((K, V) -> {
-			this.playerNames.get(K).setText(V);
-		});
-	}
-
-	public void showDices(final int dice1, final int dice2, final int dice3) {
-
+		// TODO
 	}
 
 }

c2b9350cc725da537d9f70a24b0581ebe073fa61
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-04-30 18:29:26 +0200

    Converted static methods from view utilities

--- monoopoly/src/main/java/monoopoly/view/utilities/ViewUtilities.java
+++ monoopoly/src/main/java/monoopoly/view/utilities/ViewUtilities.java
@@ -3,18 +3,9 @@ package monoopoly.view.utilities;
 import javafx.event.Event;
 import javafx.scene.layout.GridPane;
 import monoopoly.model.item.Tile;
+import monoopoly.utilities.Pair;
 
 public interface ViewUtilities {
-
-	/**
-	 * This method return the linear position of a cell in a grid
-	 * 
-	 * @param x        value
-	 * @param y        value
-	 * @param totCells number of cells
-	 * @return the linear position
-	 */
-	public int getBoardPosition(int x, int y, int totCells);
 
 	/**
 	 * This method initializes the game board setting all the names of the
@@ -30,5 +21,33 @@ public interface ViewUtilities {
 	 * @param event
 	 */
 	public void closeApp(Event event);
+
+	/**
+	 * This method returns the string representing the double value with 2 decimal
+	 * digits
+	 * 
+	 * @param number to convert in string
+	 * @return the string
+	 */
+	String toMoneyString(Double number);
+
+	/**
+	 * This methods returns the grid cell coordinates relatives to a position
+	 * 
+	 * @param position pawn position on the board
+	 * @param pane     the gridPane where the pawns are located
+	 * @return the coordinates
+	 */
+	Pair<Integer, Integer> getCoords(int position, GridPane pane);
+
+	/**
+	 * This method return the linear position of a cell in a grid
+	 * 
+	 * @param x        value
+	 * @param y        value
+	 * @param totCells number of cells
+	 * @return the linear position
+	 */
+	int getBoardPosition(int x, int y, int totCells);
 
 }
--- monoopoly/src/main/java/monoopoly/view/utilities/ViewUtilitiesImpl.java
+++ monoopoly/src/main/java/monoopoly/view/utilities/ViewUtilitiesImpl.java
@@ -1,5 +1,6 @@
 package monoopoly.view.utilities;
 
+import java.text.DecimalFormat;
 import java.util.Optional;
 
 import javafx.application.Platform;
@@ -12,30 +13,37 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import monoopoly.model.item.Table;
 import monoopoly.model.item.TableImpl;
+import monoopoly.utilities.Pair;
 
+/**
+ * This class is an utilities class for the view controllers
+ */
 public class ViewUtilitiesImpl implements ViewUtilities {
 
 	private Table table = new TableImpl();
+	private DecimalFormat format = new DecimalFormat("0.00");
 
 	@Override
 	public int getBoardPosition(int x, int y, int totCells) {
 
-		if (x == 0 || y == totCells / 2) {
-			return totCells - (x + y);
+		if (x == 0 || y == totCells / 4) {
+			return totCells / 2 - (x + y);
 		} else {
-			return totCells + (x + y);
+			return totCells / 2 + (x + y);
 		}
 	}
 
 	@Override
 	public void initializeBoard(GridPane pane) {
-		for (int i = 0; i < 11; i++) {
-			for (int j = 0; j < 11; j++) {
-				if (i == 0 || j == 0 || i == 10 || j == 10) {
-					// TODO Devo modificare la condizione per prendere la posizione (usando le
-					// dimensioni del pane passato come parametro)
+		int rows = pane.getRowCount();
+		int columns = pane.getColumnCount();
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (i == 0 || j == 0 || i == rows - 1 || j == columns - 1) {
 					Button button = this.getChild(pane, i, j);
-					String name = table.getTile(this.getBoardPosition(j, i, 20)).getName().toUpperCase();
+					String name = table.getTile(this.getBoardPosition(j, i, (rows - 1 + columns - 1) * 2)).getName()
+							.toUpperCase();
 					button.setText(name);
 				}
 			}
@@ -77,6 +85,57 @@ public class ViewUtilitiesImpl implements ViewUtilities {
 				return (Button) button;
 		}
 		return null;
+	}
+
+	@Override
+	public String toMoneyString(Double number) {
+		return "€\t" + this.format.format(number);
+	}
+
+	@Override
+	public Pair<Integer, Integer> getCoords(final int position, final GridPane pane) {
+		int rows = pane.getRowCount() - 1;
+		int columns = pane.getColumnCount() - 1;
+		int totCells = (rows + columns) * 2;
+
+		int x;
+		int y;
+
+		if (position <= totCells / 2) {
+			if (position <= columns) {
+				x = position;
+				y = 0;
+			} else {
+				x = columns;
+				y = position - columns;
+			}
+		} else {
+			if (position >= totCells - columns) {
+				x = 0;
+				y = totCells - position;
+			} else {
+				x = totCells - rows;
+				y = rows;
+			}
+
+		}
+		return revertCoords(x, y, rows, columns);
+	}
+
+	/**
+	 * This methods adapts the coordinates generated to the grid pane indexing
+	 * convention
+	 * 
+	 * @param x       value
+	 * @param y       value
+	 * @param rows    number
+	 * @param columns number
+	 * @return the coordinates of the gridCell
+	 */
+	private Pair<Integer, Integer> revertCoords(int x, int y, int rows, int columns) {
+		x = Math.abs(columns - x);
+		y = Math.abs(rows - y);
+		return new Pair<Integer, Integer>(x, y);
 	}
 
 }

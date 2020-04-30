341f1d2d0b21b74bc15face11e0594bebfe3c2fa
Cristian Fabbri <cristian.fabbri6@studio.unibo.it>
2020-04-30 16:03:12 +0200

    modified something

--- monoopoly/src/main/java/monoopoly/controller/dices/DicesObserver.java
+++ monoopoly/src/main/java/monoopoly/controller/dices/DicesObserver.java
@@ -1,17 +1,21 @@
 package monoopoly.controller.dices;
 
+import java.util.Map;
+
+import monoopoly.controller.dices.SpeedyDice.Faces;
+
 public interface DicesObserver {
 
 	/**
 	 * This method notifies the sum of the first two dices.
 	 * @param sum what came out.
 	 */
-	void notifyNormalDices(int sum);
+	void notifyNormalDices(Map<Integer, Integer> diceMap);
 	
 	/**
 	 * This method notifies the result of the roll of the speedy Dice
-	 * @param number what came out.
+	 * @param face what came out.
 	 */
-	void notifySpeedyDice(int number);
+	void notifySpeedyDice(Faces face);
 	
 }
--- monoopoly/src/main/java/monoopoly/controller/dices/SpeedyDice.java
+++ monoopoly/src/main/java/monoopoly/controller/dices/SpeedyDice.java
@@ -1,32 +1,58 @@
 package monoopoly.controller.dices;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import monoopoly.controller.player_manager.PlayerManager;
 import monoopoly.model.item.Table;
 
 public class SpeedyDice extends AbstractDiceDecorator{
+	
+	public enum Faces {
+		ONE,
+		TWO,
+		THREE,
+		BUS,
+		MR_MONOOPOLY;
+	}
 
 	private int number;
 	private final Random speedyRandom;
 	private static final int DICE_BOUND = 5;
+	private Map<Integer, Faces> facesMap;
 	
 	public SpeedyDice(Dices toDecorate) {
 		super(toDecorate);
 		this.speedyRandom = new Random();
+		initMap();
 	}
 		
+
+	private void initMap() {
+		this.facesMap = new HashMap<Integer, SpeedyDice.Faces>();
+		this.facesMap.put(1, Faces.ONE);
+		this.facesMap.put(2, Faces.TWO);
+		this.facesMap.put(3, Faces.THREE);
+		this.facesMap.put(4, Faces.BUS);
+		this.facesMap.put(5, Faces.BUS);
+		this.facesMap.put(6, Faces.MR_MONOOPOLY);
+		
+	}
+
 
 	@Override
 	public void roll() {
 		super.roll();
 		this.number = speedyRandom.nextInt(DICE_BOUND) + 1;
+		int sum = super.getDices().values().stream().reduce(0, Integer::sum) + 
+				this.number <= 3? this.number : 0;
 		this.notifyObservers();
 	}
 	
 	private void notifyObservers() {
 		for (DicesObserver obs : super.getObserverSet()) {
-			obs.notifySpeedyDice(this.number);
+			obs.notifySpeedyDice(this.facesMap.get(this.number));
 		}
 	}
 

8005cb5055c623676a3e6ce74ea02e67d5af141c
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-05-07 23:00:37 +0200

    Connected chart to controller

--- monoopoly/src/main/java/monoopoly/view/controller/StockMarketViewControllerImpl.java
+++ monoopoly/src/main/java/monoopoly/view/controller/StockMarketViewControllerImpl.java
@@ -3,9 +3,14 @@ package monoopoly.view.controller;
 import java.util.List;
 import java.util.Map;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
 import monoopoly.model.item.Tile.Category;
 
 public class StockMarketViewControllerImpl implements StockMarketViewController {
+
+	@FXML
+	private BarChart<String, Double> chart;
 
 	@Override
 	public void updateStockMarket(Map<Category, Double> stockMarket) {

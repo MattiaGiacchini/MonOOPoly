04ddc1003bc8adae4a1ea07f1c70bb720f2573fc
Mattia Giacchini <ma.giacchini99@gmail.com>
2020-05-11 21:40:19 +0200

    Loaded leaderboard fxml and its controller

---
+++ monoopoly/src/test/java/monoopoly/view/TestScoreboardViewController.java
@@ -0,0 +1,43 @@
+package monoopoly.view;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+import monoopoly.view.main.ScoreboardViewContoller;
+import monoopoly.view.main.ScoreboardViewControllerImpl;
+
+public class TestScoreboardViewController {
+
+    private ScoreboardViewContoller leaderboardController = new ScoreboardViewControllerImpl();
+
+    @Test
+    public void testOrderedLeaderboard() {
+        Map<Integer, String> names = new HashMap<Integer, String>();
+        names.put(0, "Mattia");
+        names.put(1, "Aiman");
+        names.put(2, "Daniele");
+        names.put(3, "Cristian");
+
+        Map<Integer, Double> points = new HashMap<Integer, Double>();
+        points.put(0, 500.00);
+        points.put(1, 3500.00);
+        points.put(2, 1800.00);
+        points.put(3, 900.00);
+
+        this.leaderboardController.showLeaderboard(names, points);
+        List<Entry<Integer, Double>> rank = this.leaderboardController.getRank();
+
+        assertEquals(1, rank.get(0).getKey());
+        assertEquals(2, rank.get(1).getKey());
+        assertEquals(3, rank.get(2).getKey());
+        assertEquals(0, rank.get(3).getKey());
+
+    }
+
+}
